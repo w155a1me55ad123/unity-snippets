b4822e3ec0a8c3aa0f6845a6da2b2e90ee082344
Amir Braham <amirbrahamm@gmail.com>
2018-06-06 17:27:40 +0100

    reading dummy md file , making request from react to express , displayng github-styled markdown using react-markdown and github-markdown-css

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,33 @@ import React, {
   Component
 } from 'react';
 import './App.css';
+const ReactMarkdown = require('react-markdown')
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      dummyData: ""
+    }
+  }
+  fetchMarkdown() {
+    fetch("/api/hello").then((response) => response.json())
+      .then((myJson) => {
+        this.setState({
+          dummyData: myJson.data
+        })
+      });
+  }
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React+Express Boilerplate</h1>
+          <h1 className="App-title">Welcome to Unity Snippets</h1>
+          <button onClick={this.fetchMarkdown.bind(this)} > Click me</button>
         </header>
+        <ReactMarkdown className="markdown-body" source={this.state.dummyData} />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-  path = require("path");
+  path = require("path"),
+  fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 5000; //make sure to include this port in client Packagon.json
@@ -10,12 +11,18 @@ if (process.env.NODE && ~process.env.NODE.indexOf("heroku"))
 
 
 app.get('/api/hello', (req, res) => {
-  console.log("hello");
+  fs.readFile('snippets/demo/test.md', function(err, data) {
+    if (err) return console.error(err);
+    res.json({
+      data: data.toString()
+    });
+  });
 });
 
 
 
 app.get('*', (req, res) => {
+
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
